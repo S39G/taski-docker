fadb7efc0b44cd768cd00328de9b3f88dd4ca7b5
Zhukov Sergey <sega.zhukov.7@yandex.ru>
2026-02-22 20:28:16 +0200

    Fixed error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,6 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://taskiya.hopto.org',
+]
+
 ALLOWED_HOSTS = ['localhost', '158.160.178.6', 'taskiya.hopto.org']
 
 
